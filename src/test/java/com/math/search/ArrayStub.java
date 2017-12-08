c7524f433bdc56ac8b85d240d499d3958b2bf7ae
befern <belen.fdez.glez@gmail.com>
2017-12-08 21:07:11 +0100

    Add to ArrayStub non negative step exception

--- src/test/java/com/math/search/ArrayStub.java
+++ src/test/java/com/math/search/ArrayStub.java
@@ -3,7 +3,9 @@ package com.math.search;
 public class ArrayStub {
 
     public static int[] createMonotoneIncreasingArray(int start, int end, int step) {
-        int[] resultArray = new int[(start + end) / step];
+        if (step <= 0) throw new IllegalArgumentException("Step cannot be lower than 1 in a monotone increasing array");
+
+        int[] resultArray = new int[(end - start) / step];
 
         for (int i = 0; i < resultArray.length; i++) resultArray[i] = i * step + start;
 
